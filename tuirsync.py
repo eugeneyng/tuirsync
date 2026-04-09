941afcfb111375f9c00a04c23aaa95fbc5cdd45c
eugene <ngyeugene@gmail.com>
2026-04-09 00:17:22 -0400

    added menu, ssh connection screens

--- tuirsync.py
+++ tuirsync.py
@@ -5,9 +5,9 @@ import textual.widgets
 
 class RSyncApp(textual.app.App):
     BINDINGS = [
+        ("escape,q", "quit", "Quit"),
         ("d", "dark_toggle", "Toggle Dark Mode"),
-        ("m", "menu", "Toggle Menu"),
-        ("q", "quit", "Quit"),
+        ("m", "menu_toggle", "Toggle Menu"),
     ]
 
     def compose(self) -> textual.app.ComposeResult:
@@ -25,18 +25,68 @@ class RSyncApp(textual.app.App):
 
 
 class Menu(textual.screen.ModalScreen):
+    BINDINGS = [("escape,q", "dismiss", "Close Menu")]
+
+    CSS = """
+    Menu { align: center middle; }
+    Menu > Container {
+        width: 50%;
+        height: 75%;
+    }
+    """
+
+    def action_dismiss(self) -> None:
+        self.dismiss()
+
     def compose(self) -> textual.app.ComposeResult:
+        yield textual.widgets.Header()
+        yield textual.widgets.Footer()
         with textual.containers.Container():
             yield textual.widgets.Label("Menu")
             yield textual.widgets.ListView(
+                textual.widgets.ListItem(
+                    textual.widgets.Label("0. Settings Save Path")
+                ),
                 textual.widgets.ListItem(textual.widgets.Label("1. New Connection")),
                 textual.widgets.ListItem(textual.widgets.Label("2. Load Connection")),
             )
+
+    def on_list_view_selected(self, event: textual.widgets.ListView.Selected) -> None:
+        # Handle menu item selection
+
+        if event.list_view.index == 1:
+            self.app.push_screen(SSHConnectionScreen())
+
+
+class SSHConnectionScreen(textual.screen.ModalScreen):
+    BINDINGS = [("escape,q", "dismiss", "Close SSH Connection Screen")]
+
+    CSS = """
+    SSHConnectionScreen { align: center middle; }
+    SSHConnectionScreen > Container {
+        width: 50%;
+        height: 75%;
+    }
+    """
+
+    def on_mount(self):
+        self.notify("New SSH Connection...")
+
+    def compose(self) -> textual.app.ComposeResult:
+        yield textual.widgets.Header()
+        yield textual.widgets.Footer()
+        with textual.containers.Container():
+            yield textual.widgets.Label("SSH Connection Settings")
 
 
 class Trees(textual.containers.Horizontal):
     def compose(self) -> textual.app.ComposeResult:
         yield textual.widgets.DirectoryTree("./")
+        yield textual.widgets.DirectoryTree("./")
+
+
+class RemoteTree(textual.widgets.Tree):
+    def compose(self) -> textual.app.ComposeResult:
         yield textual.widgets.DirectoryTree("./")
 
 
